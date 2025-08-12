7b3539d20d5567cec4f4585a765f9fa6f73a692c
Dailin2025 <djia0020@student.monash.edu>
2025-08-12 17:01:34 +0800

    Fix ES module compatibility: restore module type and add Babel support for Vue CLI

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/NoMash-Library/'
-    : '/'
+    : '/',
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
+  }
 } 
